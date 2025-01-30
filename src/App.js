6cf240c7a8a62b664f90003e70895a04010daf12
ARBUCHELI <radio_seattle@hotmail.com>
2025-01-30 11:46:47 +0300

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
+const contentDefault = `
+# Welcome to the React Markdown Previewer!
+
+## This is a sub-heading...
+
+### Here's more cool stuff:
+
+Inline code: \`<div></div>\`
+
+\`\`\`javascript
+// Multi-line code example:
+function example() {
+  console.log("Hello, world!");
+}
+\`\`\`
+
+**Bold**, _italic_, or **_both_**!
+
+> Block Quotes are fun!
+
+1. Numbered lists.
+2. Easy to create.
+3. Very useful.
+
+- Bullet points
+  - Nested items
+    - More nesting
+
+[Visit my portfolio](https://bucheli-web-personal-portfolio-website.netlify.app)
+
+![Sample Logo](https://bucheli-web-personal-portfolio-website.netlify.app/company-logo-6.png)
+`;
+
 function App() {
+  const [content, setContent] = useState(contentDefault);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 id="inner_header">REACT MARKDOWN PREVIEWER</h1>
+      <div className="markdown-container">
+        <textarea
+          id="editor"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(content) }}
+        />
+      </div>
     </div>
   );
 }
